01d692e9a04e0976719fae70a73511bcdab8c957
Felipe Kutz <felipekutz@hotmail.com>
2021-05-24 20:24:11 -0300

    third commit

--- src/components/meetups/NewMeetup.js
+++ src/components/meetups/NewMeetup.js
@@ -1,32 +1,60 @@
+import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetup.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription
+    };
+    console.log(meetupData)
+    props.onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="image">Image</label>
-          <input type="url" required id="url" />
+          <input type="url" required id="url" ref={imageInputRef} />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5" />
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          />
         </div>
 
         <div className={classes.actions}>
-          <button type='submit'>Add meetup</button>
+          <button type="submit">Add meetup</button>
         </div>
       </form>
     </Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,18 @@
 import NewMeetupForm from "../components/meetups/NewMeetup";
 
 function NewMeetupPage(){
+    function addMeetupHandler(meetupData){
+        fetch('https://react-getting-started-d7957-default-rtdb.firebaseio.com/meetups.json',{
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section> 
     
 }
